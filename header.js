11e8dcaadbb40ee9a93fa087481e3f7c9b65de67
shkwahab <wahab209266@gmail.com>
2022-12-24 01:22:27 +0500

    Landing Page

---
+++ header.js
@@ -0,0 +1,19 @@
+
+
+$("#mobilebtn").click(function(){
+    $("#mobile-menu-2").toggleClass("hidden");
+})
+$("#copyright").html("&copy; CopyRights " + new Date().getFullYear() + " " + $("#copyright").html());
+$("#q1opener").click(function(){
+    $("#q1").toggleClass("hidden");
+})
+$("#q2opener").click(function(){
+    $("#q2").toggleClass("hidden");
+})
+$("#q3opener").click(function(){
+    $("#q3").toggleClass("hidden");
+})
+
+$("#q4opener").click(function(){
+    $("#q4").toggleClass("hidden");
+})
